a5475aeb78d382af2f4ed79169b11cd659b2d7e6
andrea <andreamtang@gmail.com>
2019-05-09 17:31:27 -0700

    colour scheme created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
 
 function Aggregate() {
@@ -10,12 +9,32 @@ function Aggregate() {
   )
 }
 
+function Filter() {
+  return (
+    <div>
+      <img/>
+      <input type="text"/>
+      Filter
+    </div>
+  )
+}
+
+function Playlist() {
+  return (
+    <div style={{color: '#2E2E2F'}}>
+      <h3>Playlist Name</h3>
+      <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+    </div>
+  )
+}
+
 function App() {  
   return (
     <div className="App-title">
       <h1>neue.</h1>
-      <Aggregate/>
-      <Aggregate/>
+      <Aggregate/> 
+      <Filter/>
+      <Playlist/>
     </div>
   );
 }
